fdabfc2543fa4e49f59f4287f3ec14f038155608
Andy Gimma <andrewgimma@protonmail.com>
2022-03-15 09:31:06 -0300

    add tailwind and daisyui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="dropdown">
+    <label tabindex="0" class="m-1 btn">Click</label>
+    <ul tabindex="0" 
+        class={`p-2 shadow menu dropdown-content 
+        bg-base-100 rounded-box w-52`}
+    >
+      <li><a>Item 1</a></li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
+  
